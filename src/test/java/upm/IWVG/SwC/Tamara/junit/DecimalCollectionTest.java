7a5c3ee777992b4fa79bce4d37b4f6a44edb64f8
TamaraJDz <TamaraJDz>
2016-09-24 14:01:22 +0200

    Primer commit

---
+++ src/test/java/upm/IWVG/SwC/Tamara/junit/DecimalCollectionTest.java
@@ -0,0 +1,44 @@
+package upm.IWVG.SwC.Tamara.junit;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import upm.IWVG.SwC.Tamara.DecimalCollection;
+
+public class DecimalCollectionTest extends TestCase {
+
+	private DecimalCollection dc;
+
+    @Before
+    public void before() {
+        this.dc = new DecimalCollection();
+        this.dc.add(2.0);
+        this.dc.add(-1.0);
+        this.dc.add(3.0);
+        this.dc.add(2.0);
+    }
+
+    @Test
+    public void testDecimalCollection() {
+        this.dc = new DecimalCollection();
+        assertEquals(0, this.dc.size());
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(4, this.dc.size());
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(6, this.dc.sum(), 10e-5);
+    }
+
+    @Test
+    public void testHigher() {
+        assertEquals(3, this.dc.higher(), 10e-5);
+    }
+
+}
---
+++ src/test/java/upm/IWVG/SwC/Tamara/junit/FractionTest.java
@@ -0,0 +1,47 @@
+package upm.IWVG.SwC.Tamara.junit;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import upm.IWVG.SwC.Tamara.Fraction;
+
+public class FractionTest extends TestCase {
+
+	 private Fraction fr;
+		
+	    @Before
+	    public void before() {
+	        fr = new Fraction(25, 5);
+	    }
+		
+		@Test
+		public void testFractionIntInt() {
+			assertEquals(25, fr.getNumerator());
+	        assertEquals(5, fr.getDenominator());
+		}
+
+		@Test
+		public void testFraction() {
+			fr = new Fraction();
+	        assertEquals(1, fr.getNumerator());
+	        assertEquals(1, fr.getDenominator());
+		}
+
+		@Test
+		public void testDecimal() {
+			assertEquals(5, fr.getNumerator() / fr.getDenominator());
+		}
+
+		@Test
+		public void testGetNumerator() {
+			assertEquals(25, fr.getNumerator());
+		}
+
+		@Test
+		public void testGetDenominator() {
+			assertEquals(5, fr.getDenominator());
+		}
+
+}
