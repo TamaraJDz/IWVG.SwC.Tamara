e51c29093369ab57fed2f4d54b5302a077e56fe5
miguel <ma.bernabecruz@gmail.com>
2016-10-03 14:02:30 +0200

    Implementados nuevos métodos de la clase Point modificar coordenadas #1.

--- src/main/java/upm/IWVG/SwC/Tamara/Point.java
+++ src/main/java/upm/IWVG/SwC/Tamara/Point.java
@@ -37,6 +37,14 @@ public class Point {
     public int getY() {
         return this.y;
     }
+    
+    public void setX(int x) {
+        this.x = x ;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     @Override
     public String toString() {
